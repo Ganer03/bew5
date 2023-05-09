4f7ed5fab90ad94a53296d6be082d201e46a014a
Ganer03 <112845740+Ganer03@users.noreply.github.com>
2023-05-10 01:55:34 +0300

    Update index.php

--- index.php
+++ index.php
@@ -125,7 +125,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
     // Если нет предыдущих ошибок ввода, есть кука сессии, начали сессию и
     // ранее в сессию записан факт успешного логина.
-    if (empty($errors) && !empty($_COOKIE[session_name()]) &&
+    if (count(array_filter($errors)) === 0 && !empty($_COOKIE[session_name()]) &&
         session_start() && !empty($_SESSION['login'])) {
         // TODO: загрузить данные пользователя из БД
         // и заполнить переменную $values,
